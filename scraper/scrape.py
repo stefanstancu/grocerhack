a6d6b7d452566d6fe51ad756e042d2c125a8674b
Stefan Stancu <stefan.stancu15@gmail.com>
2019-04-04 23:33:10 -0400

    we got banned from the api

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,5 +1,6 @@
 import sys
-import requests as r
+import aiohttp
+import asyncio
 from string import ascii_uppercase
 
 
@@ -7,22 +8,40 @@ url_base = 'https://www.loblaws.ca/api/product/'
 headers = { 'Site-Banner':'loblaw' }
 letter_endings = [ 'KG', 'EA']
 
+file_name = 'test_codes.txt'
+starting_int = 20154836
+ending_int = 20154841
 
-starting_int = 20060000
+async def fetch(session, code):
+    url = url_base + code;
+    async with session.get(url, headers=headers) as resp:
+        data = []
+        print(code, end='\r')
+        print(await resp.text())
+        if 'code' in data:
+            print(data['name'])
+            with open(file_name, 'a+') as f:
+                f.write(code)
+
+async def fetch_codes(codes):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for code in codes:
+            task = asyncio.ensure_future(fetch(session, code))
+            tasks.append(task)
+        await asyncio.gather(*tasks, return_exceptions=True)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        ending = sys.argv[1]
-        letter_endings = [ending]
-        starting_int = int(sys.argv[2])
+        starting_int = int(sys.argv[1])
 
-    for i in range(starting_int, 21000000):
+    codes = []
+    for i in range(starting_int, ending_int):
         for ending in letter_endings:
-            url = url_base + str(i) + "_" + ending 
-            resp = r.get(url, headers=headers).json()
-            print(str(i) + "_" + ending, end='\r')
-            if 'code' in resp:
-                print('found ' + resp['name'])
-                with open('codes.txt', 'a+') as f:
-                    f.write(resp['code'] + "\n")
+            code = str(i) + "_" + ending 
+            codes.append(code)
+
+    print(f'generated {len(codes)} codes')
+
+    asyncio.run(fetch_codes(codes))
 
