416ac4289e7ac40409e180dd6190dae9406d3b49
Stefan Stancu <stefan@stancu.co>
2019-04-05 17:03:35 -0400

    tuner done, opt found

--- scraper/scraper_tuner.py
+++ scraper/scraper_tuner.py
@@ -3,9 +3,10 @@ from scrape import fetch_codes, generate_codes
 import asyncio
 import time
 
-max_step = 1000
-test_range = 10000
+max_step = 72
+test_range = 100
 starting_int = 20154836
+batch_sleep = 0.01
 
 def test_step(step, codes):
     print(f'testing {len(codes)} with batch_size={step}')
@@ -18,16 +19,17 @@ def test_step(step, codes):
         except Exception as e:
             print(e)
             return False
+        sleep(batch_sleep)
     return True
 
 def test_bounds(upper, lower, codes):
-    if lower < upper:
+    if lower < upper-1:
         step = int((upper - lower)/2 + lower)
         a = test_step(step, codes)
         if a:
             return test_bounds(upper, step, codes)
         else:
-            sleep(60*10)
+            sleep(5)
             return test_bounds(step, lower, codes)
     else:
         return lower
@@ -47,5 +49,5 @@ if __name__=='__main__':
 
     # find the step size using binary search
     opt_step = test_bounds(max_step, 0, codes)
-    print(f'optimal step size is {a}')
+    print(f'optimal step size is {opt_step}')
     
